29ed1d9489bdcc439483cfac62c9e8b9d0a785ed
123renxiaoyao <lwt13331262268@163.com>
2023-10-15 16:41:42 +0800

    HelloWorld master 1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
+
+/*
+All code in Java must be a part of class
+We delimit the beginning and end of segments of code using {and}
+ */
